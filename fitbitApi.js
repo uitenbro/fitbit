4030cba6f3432dff6d898bc5d4483551d8be4759
Nate Uitenbroek <nate.uitenbroek@nasa.gov>
2024-01-13 09:55:14 -0600

    all apis working for intial setup

--- fitbitApi.js
+++ fitbitApi.js
@@ -12,9 +12,16 @@ function makeApiRequest(endpoint, method = 'GET') {
     method: method,
     headers: {
       'Authorization': `Bearer ${accessToken}`,
+      'accept-language': 'en_US',
+      'accept-locale': 'en_US'
     },
   })
-    .then(response => response.json())
+    .then(response => {
+      // response.headers.forEach((value, name) => {
+      //   console.log(`Header: ${name} = ${value}`);
+      // });
+      return response.json()
+    })
     .catch(error => {
       console.error('Error making API request:', error);
       throw error;
@@ -37,24 +44,180 @@ function handleProfile(data) {
   console.log('Profile data:', data);
 }
 
-// Function to get daily activity summary from API
-function getActivityLogs(date) {
-  if (checkFitBitAccessToken('getActivityLogs', Array.from(arguments))) {
-    makeApiRequest(`activities/date/${date}.json`)
-      .then(data => handleActivityLogs(data))
-      .catch(error => console.error('Error getting activity:', error));
+// Function to get daily Calories Burned Log data from API
+function getCalorieLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getCalorieLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/calories/date/${startDate}/${endDate}.json`)
+    // makeApiRequest(`activities/activityCalories/date/${startDate}/${endDate}.json`)
+    // https://dev.fitbit.com/build/reference/web-api/activity-timeseries/get-activity-timeseries-by-date-range/#Resource-Options
+      .then(data => handleCalorieLogs(data))
+      .catch(error => console.error('Error getting calories:', error));
   }
 }
 
-// Function to handle activity data
-function handleActivityLogs(data) {
-  console.log('Activity Log data:', data);
+// Function to handle calorie data
+function handleCalorieLogs(data) {
+  console.log('Calories Burned Log data:', data);
 }
 
-// Function to get sleep logs from API
-function getSleepLogs(date) {
+// Function to get AZM Log data from API
+function getAzmLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getAzmLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/active-zone-minutes/date/${startDate}/${endDate}.json`)
+    // makeApiRequest(`activities/activityCalories/date/${startDate}/${endDate}.json`)
+    // https://dev.fitbit.com/build/reference/web-api/activity-timeseries/get-activity-timeseries-by-date-range/#Resource-Options
+      .then(data => handleAzmLogs(data))
+      .catch(error => console.error('Error getting calories:', error));
+  }
+}
+
+// Function to handle calorie data
+function handleAzmLogs(data) {
+  console.log('AZM Log data:', data);
+}
+
+
+// Function to get weight logs from API
+function getWeightLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getWeightLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`body/weight/date/${startDate}/${endDate}.json`)
+    // makeApiRequest(`body/fat/date//date/${startDate}/${endDate}.json`)
+    // https://dev.fitbit.com/build/reference/web-api/body-timeseries/get-body-timeseries-by-date-range/      .then(data => handleActivityLogs(data))
+      .then(data => handleWeightLogs(data))
+      .catch(error => console.error('Error getting weight:', error));
+  }
+}
+
+// Function to handle weight data
+function handleWeightLogs(data) {
+  console.log('Weight Log data:', data);
+}
+
+// Function to get daily step log data from API
+function getStepLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getStepLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/steps/date/${startDate}/${endDate}.json`)
+      .then(data => handleStepLogs(data))
+      .catch(error => console.error('Error getting steps:', error));
+  }
+}
+
+// Function to handle step data
+function handleStepLogs(data) {
+  console.log('Step Log data:', data);
+}
+
+// Function to get daily distance log data from API
+function getDistanceLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getDistanceLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/distance/date/${startDate}/${endDate}.json`)
+      .then(data => handleDistanceLogs(data))
+      .catch(error => console.error('Error getting distance:', error));
+  }
+}
+
+// Function to handle distance data
+function handleDistanceLogs(data) {
+  console.log('Distance Log data:', data);
+}
+
+
+// Function to get daily minutes sedentary log data from API
+function getMinutesSedentaryLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getMinutesSedentaryLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/minutesSedentary/date/${startDate}/${endDate}.json`)
+      .then(data => handleMinutesSedentaryLogs(data))
+      .catch(error => console.error('Error getting minutes sedentary:', error));
+  }
+}
+
+// Function to handle minutes sedentary data
+function handleMinutesSedentaryLogs(data) {
+  console.log('Minutes Sedentary Log data:', data);
+}
+
+// Function to get daily minutes lightly active log data from API
+function getMinutesLightlyActiveLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getMinutesLightlyActiveLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/minutesLightlyActive/date/${startDate}/${endDate}.json`)
+      .then(data => handleMinutesLightlyActiveLogs(data))
+      .catch(error => console.error('Error getting minutes lightly active:', error));
+  }
+}
+
+// Function to handle minutes lightly active data
+function handleMinutesLightlyActiveLogs(data) {
+  console.log('Minutes Lightly Active Log data:', data);
+}
+
+// Function to get daily minutes fairly active log data from API
+function getMinutesFairlyActiveLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getMinutesFairlyActiveLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/minutesFairlyActive/date/${startDate}/${endDate}.json`)
+      .then(data => handleMinutesFairlyActiveLogs(data))
+      .catch(error => console.error('Error getting minutes fairly active:', error));
+  }
+}
+
+// Function to get daily minutes fairly active log data from API
+function getMinutesFairlyActiveLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getMinutesFairlyActiveLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/minutesFairlyActive/date/${startDate}/${endDate}.json`)
+      .then(data => handleMinutesFairlyActiveLogs(data))
+      .catch(error => console.error('Error getting minutes fairly active:', error));
+  }
+}
+
+// Function to handle minutes fairly active data
+function handleMinutesFairlyActiveLogs(data) {
+  console.log('Minutes Fairly Active Log data:', data);
+}
+
+// Function to get daily minutes very active log data from API
+function getMinutesVeryActiveLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getMinutesVeryActiveLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`activities/minutesVeryActive/date/${startDate}/${endDate}.json`)
+      .then(data => handleMinutesVeryActiveLogs(data))
+      .catch(error => console.error('Error getting minutes very active:', error));
+  }
+}
+
+// Function to handle minutes very active data
+function handleMinutesVeryActiveLogs(data) {
+  console.log('Minutes Very Active Log data:', data);
+}
+
+// Function to get fat percent logs from API
+function getFatPercentLogs(startDate, duration) {
+  if (checkFitBitAccessToken('getWeightLogs', Array.from(arguments))) {
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`body/fat/date/${startDate}/${endDate}.json`)
+      .then(data => handleFatPercentLogs(data))
+      .catch(error => console.error('Error getting fat percent:', error));
+  }
+}
+
+// Function to handle weight data
+function handleFatPercentLogs(data) {
+  console.log('Fat Percent Log data:', data);
+}
+
+// Function to get sleep logs from API with duration
+function getSleepLogs(startDate, duration) {
   if (checkFitBitAccessToken('getSleepLogs', Array.from(arguments))) {
-    makeApiRequest(`sleep/date/${date}.json`)
+    const endDate = calculateEndDate(startDate, duration);
+    const apiUrl = `/sleep/date/${startDate}/${endDate}.json`;
+    makeApiRequest(apiUrl)
       .then(data => handleSleepLogs(data))
       .catch(error => console.error('Error getting sleep logs:', error));
   }
@@ -66,15 +229,49 @@ function handleSleepLogs(data) {
 }
 
 // Function to get food log from API for a specific date
-function getFoodLogs(date) {
+function getFoodLogs(startDate, duration) {
   if (checkFitBitAccessToken('getFoodLogs', Array.from(arguments))) {
-    makeApiRequest(`foods/log/date/${date}.json`)
+    const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`foods/log/caloriesIn/date/${startDate}/${endDate}.json`)
       .then(data => handleGetFoodLogs(data))
-      .catch(error => console.error('Error getting diet macros:', error));
+      .catch(error => console.error('Error getting food log:', error));
   }
 }
 
 // Function to handle food log data
 function handleGetFoodLogs(data) {
   console.log('Food log data:', data);
+}
+
+// Function to get diet macro log from API for a specific date
+function getMacroLogs(startDate, duration=1) {
+  if (checkFitBitAccessToken('getMacroLogs', Array.from(arguments))) {
+    //const endDate = calculateEndDate(startDate, duration);
+    makeApiRequest(`foods/log/date/${startDate}.json`)
+      .then(data => handleGetMacroLogs(data))
+      .catch(error => console.error('Error getting diet macros:', error));
+  }
+}
+
+// Function to handle diet macro log data
+function handleGetMacroLogs(data) {
+  console.log('Diet Macro log data:', data);
+}
+
+// Function to get start date from the input
+function getStartDate() {
+  return document.getElementById('startDate').value;
+}
+
+// Function to get duration from the input
+function getDuration() {
+  return parseInt(document.getElementById('duration').value, 10);
+}
+
+// Function to calculate end date based on start date and duration
+function calculateEndDate(startDate, duration) {
+  const start = new Date(startDate);
+  const end = new Date(start);
+  end.setDate(start.getDate() + duration);
+  return end.toISOString().split('T')[0];
 }
